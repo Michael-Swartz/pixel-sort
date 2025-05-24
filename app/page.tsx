673575f9b1d0d017ef851ca3cb68121dea3765e0
Michael Swartz <mswartz91095@outlook.com>
2025-05-24 05:34:01 -0400

    Update header in Home component to reflect a more concise project title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import P5Sketch from './components/P5Sketch';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold mb-8">Scooters Pixel Sorter</h1>
+      <h1 className="text-4xl font-bold mb-8">Pixel Sorter</h1>
       <P5Sketch />
     </div>
   );
